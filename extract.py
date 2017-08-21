4f0d5d90e5bb7ec47e6adc2218feddfafe16d2a6
Dhananjoy Biswas <ebappa1971@gmail.com>
2017-08-21 23:46:02 +0600

    Move nodesSlector to Values

--- extract.py
+++ extract.py
@@ -1,6 +1,5 @@
 for y in open('all.yml').read().split('---')[1:]:
 	y = filter(lambda s: s.strip(), y.split('\n'))
 	n = y[0][34:]
-	with open(n, 'w') as f:
+	with open('build/' + n, 'w') as f:
 		f.write('\n'.join(y) + '\n')
-
